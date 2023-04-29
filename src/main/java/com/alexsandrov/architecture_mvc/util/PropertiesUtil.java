241372ffc2e771a09bc461d22a9b7ff27173a0e7
Morrrun <mifon.199708@gmail.com>
2023-04-29 09:55:38 +0400

    Доработка PropertiesUtil

--- src/main/java/com/alexsandrov/architecture_mvc/util/PropertiesUtil.java
+++ src/main/java/com/alexsandrov/architecture_mvc/util/PropertiesUtil.java
@@ -1,6 +1,8 @@
 package com.alexsandrov.architecture_mvc.util;
 
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 /**
@@ -9,10 +11,39 @@ import java.util.Properties;
  * по ключу указанному в файле application.properties
  */
 public final class PropertiesUtil {
+
+    /**
+     * Переменная PROPERTIES хранит загруженные конфигурации
+     */
     private static final Properties PROPERTIES = new Properties();
+
     private PropertiesUtil() {
     }
 
+    /**
+     * Статический блок для вызова загрузки конфигураций
+     */
+    static {
+        loadProperties();
+    }
+
+    /**
+     * Статический метод для загрузки конфигураций
+     */
+    private static void loadProperties() {
+        try (InputStream resourceAsStream = PropertiesUtil.class
+                .getClassLoader()
+                .getResourceAsStream("application.properties")) {
+
+            PROPERTIES.load(resourceAsStream);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    /**
+     * Статический метод для получения конфигурации по ключу
+     */
     private static String get(String key) {
         return PROPERTIES.getProperty(key);
     }
