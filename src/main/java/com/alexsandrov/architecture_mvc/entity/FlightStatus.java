ef1b663df3ea2609361a00315db898d12f424e4a
Morrrun <mifon.199708@gmail.com>
2023-04-29 09:36:57 +0400

    Initial commit 3

---
+++ src/main/java/com/alexsandrov/architecture_mvc/entity/FlightStatus.java
@@ -0,0 +1,8 @@
+package com.alexsandrov.architecture_mvc.entity;
+
+public enum FlightStatus {
+    ARRIVED,
+    DEPARTED,
+    CANCELLED,
+    SCHEDULED
+}
---
+++ src/main/java/com/alexsandrov/architecture_mvc/util/ConnectionManager.java
@@ -0,0 +1,13 @@
+package com.alexsandrov.architecture_mvc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public final class ConnectionManager {
+
+    private ConnectionManager() {
+    }
+//     public static Connection get() {
+//        return DriverManager.getConnection("url", "user", "password");
+//     }
+}
